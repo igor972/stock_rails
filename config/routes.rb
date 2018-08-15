4a024413ef3019f91742801111b5d95a4e9acc35
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2018-08-15 15:05:04 +0000

    scaffold reasons and warehouses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :reasons
+  resources :warehouses
   root 'welcome#index'
   resources :products
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_04_054458) do
+ActiveRecord::Schema.define(version: 2018_08_14_200340) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2018_08_04_054458) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "warehouses", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "reason_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reason_id"], name: "index_warehouses_on_reason_id"
   end
 
 end
