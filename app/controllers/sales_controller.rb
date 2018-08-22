2a9260251d6613678130f4c5ab816223f7a9de94
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2018-08-22 16:28:48 +0000

    Controller Sales created

---
+++ app/controllers/sales_controller.rb
@@ -0,0 +1,45 @@
+class SalesController < ApplicationController
+  before_action :set_sale, only: [:show, :destroy]
+
+  def index
+    @sale = Sale.all
+  end
+
+  def show
+  end
+
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = WarehouseChange.new(sale_params)
+
+    respond_to do |format|
+      if @sale.save
+        format.html { redirect_to @sale, notice: 'Sale was successfully created.' }
+        format.json { render :show, status: :created, location: @sale }
+      else
+        format.html { render :new }
+        format.json { render json: @sale.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @sale.destroy
+    respond_to do |format|
+      format.html { redirect_to sales_url, notice: 'Sale was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_sale
+      @sale = Sale.find(params[:id])
+    end
+
+    def sales_params
+      require(:sale).permit(:total)
+    end
+end
