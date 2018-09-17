1c9053f6da83174222374c95b8411e35214c218b
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2018-09-17 18:28:22 +0000

    report controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :reasons
   root 'welcome#index'
   resources :products
-
+  get 'reports', to: 'reports#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
