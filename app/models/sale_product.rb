f3e3b70bfb2d0e3c54933389d65a0b17556e1a1f
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2018-09-25 07:35:49 +0000

    add validations to sale product addition

--- app/models/sale_product.rb
+++ app/models/sale_product.rb
@@ -3,10 +3,11 @@ class SaleProduct < ApplicationRecord
   belongs_to :product
 
   validates :quantity, numericality: { greater_than: 0 }
-  validate :available_quantity
+
+  before_validation :cant_be_negative, on: :available_quantity
+
   before_create :add_change_warehouse
   before_save :set_current_price
-
 
   # FIX: usar o current_price
   def subtotal
@@ -28,5 +29,17 @@ class SaleProduct < ApplicationRecord
 
   def set_current_price
     self.current_price = subtotal
+  end
+
+  def cant_be_negative
+    unless is_number?(self.quantity)
+      errors.add(:quantity, "Deve ser número válido") 
+      return ''
+    end
+
+    if self.quantity < 1
+      errors.add(:quantity, "Deve ser maior que 0")
+      return ''
+    end
   end
 end
