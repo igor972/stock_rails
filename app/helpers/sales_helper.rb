9637035199d7c0d251896d8da0138010b6f78d92
Igor Oliveira <igoroliveira.oliveira@gmail.com>
2018-09-28 11:12:28 +0000

    Show status on Sales Page

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -2,4 +2,14 @@ module SalesHelper
   def get_all_active_sales
     Sale.actives_sales
   end
+
+  def sale_status(sale)
+    ret = ""
+    if sale.active
+      ret = "<span class='badge badge-info'>Ativo</span>"
+    else
+      ret = "<span class='badge badge-danger'>Cancelado</span>"
+    end
+    ret.html_safe
+  end
 end
